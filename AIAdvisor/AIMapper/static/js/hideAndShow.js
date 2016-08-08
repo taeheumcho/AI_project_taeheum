f32b577211b61a32d510f795ecebde6cba229ee2
thCho <taeheumcho@gmail.com>
2016-08-08 14:33:55 +0900

    ggg

--- AIAdvisor/AIMapper/static/js/hideAndShow.js
+++ AIAdvisor/AIMapper/static/js/hideAndShow.js
@@ -1,27 +1,8 @@
-var a=document.getElementById('execute');
-var b=document.getElementById('reset');
-$('execute').on('click',function(){
-    document.getElementById('execute_form').style.display="";
-});
-a.on('click',function(){
-    document.getElementById('execute_form').style.display="";
-});
-b.on('click',function(){
-	document.getElementById('execute_form').style.display="none";
-//    document.getElementById('b_form').style.display="";
-});
-
-//function visiblity_showAndHid(showid) {
-//	var e = document.getElementById(showid);
-//	if(showid = 'execute')
-////		if(document.getElementById('execute_form').style.display = '')
-////			
-////		else
-//			document.getElementById('execute_form').style.display = '';
-//	else
-////		if(document.getElementById('execute_form').style.display = 'none')
-////		
-////		else
-//			document.getElementById('execute_form').style.display = 'none';
-//	
-//}
+function visiblity_showAndHid(showid) {
+	var e = document.getElementById(showid);
+	if(showid == 'execute')
+			document.getElementById('execute_form').style.display = "";
+	else
+			document.getElementById('execute_form').style.display = "none";
+	
+}
