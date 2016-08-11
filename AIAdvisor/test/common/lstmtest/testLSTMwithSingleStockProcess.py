f5218c6f9711a087de30ca7587a97def6575401e
thcho <thcho@taeheumcho>
2016-08-11 17:32:09 +0900

    testestest

---
+++ AIAdvisor/test/common/lstmtest/testLSTMwithSingleStockProcess.py
@@ -0,0 +1,15 @@
+'''
+Created on 2016. 8. 11.
+
+@author: thcho
+'''
+# use trade amount, moving average value, high and low values, final price as an input parameters
+# out put is the probability of raise of stock [ex. up : 1, down : 0]
+# forcast 1 week stock prices : multiple perceptron LSTM
+# give weights on dates just after any holidays
+# only for single stock analysis
+
+
+
+
+
